5c937fe59720623575fa78f087a9deffcd90fb94
v.rysin <666728666zbk>
2022-04-11 17:52:36 +0400

    feat: 2.8. Анимируем слоган
    скрипт анимации

---
+++ source/js/animateLetters.js
@@ -0,0 +1,39 @@
+const SPAN = `span`;
+const ANIMATED_WORD = `animated-word`;
+const ANIMATED_LETTER = `animated-letter`;
+const ANIMATED = `animated-letters`;
+
+export default (node, params) => {
+  if (!node) {
+    throw new Error(`node is required for animateLetters`);
+  }
+
+  const {textContent} = node;
+  const arrayOfLines = textContent.split(` `);
+
+  node.innerHTML = null;
+  node.classList.add(ANIMATED);
+
+  arrayOfLines.forEach((line, i) => {
+    const spanWord = document.createElement(SPAN);
+    spanWord.classList.add(ANIMATED_WORD);
+
+    const lineChars = line.split(``);
+
+    lineChars.forEach((char) => {
+      const spanChar = document.createElement(SPAN);
+      spanChar.classList.add(ANIMATED_LETTER);
+      spanChar.innerHTML = char;
+
+      spanChar.style.animation = params.animation;
+      spanChar.style.animationFillMode = params.animationFillMode;
+      spanChar.style.animationDuration = `${params.duration}ms`;
+      spanChar.style.animationDelay = `${(Math.random() + 1) * params.delay * (i + 1)}ms`;
+      spanChar.style.animationTimingFunction = params.animationTimingFunction;
+
+      spanWord.appendChild(spanChar);
+    });
+
+    node.appendChild(spanWord);
+  });
+};
--- source/js/script.js
+++ source/js/script.js
@@ -8,6 +8,7 @@ import result from './modules/result.js';
 import form from './modules/form.js';
 import social from './modules/social.js';
 import FullPageScroll from './modules/full-page-scroll';
+import mainPage from "./modules/mainPage";
 
 // init modules
 mobileHeight();
@@ -18,6 +19,7 @@ chat();
 result();
 form();
 social();
+mainPage();
 
 const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
